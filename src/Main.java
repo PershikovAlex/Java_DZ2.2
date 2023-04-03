5fb8ca38586dfd4b53279f0a3b7598d3b4969991
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-04-03 11:09:09 +0500

    Add calculation

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;  //Начальный баланс клиента
+        int payment = 1100; //Сумма пополнения
+        int totalAmount;    //Итоговый баланс
+        int gift;           //Количество бонусов для начисления
+
+        if (payment >= 1000){
+            gift = payment / 100;
+            totalAmount = balance + payment + gift;
+            System.out.println("Ваш бонус составил: " + gift);
+            System.out.println("Ваш баланс: " + totalAmount);
+        } else {
+            totalAmount = balance + payment;
+            System.out.println("Ваш баланс: " + totalAmount);
+        }
+    }
+}
